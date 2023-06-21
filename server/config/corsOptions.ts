86eca4e0580e89a8cd1da0e522197574d4ef4a6b
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-06-21 13:01:40 +0200

    Added cors options

---
+++ server/config/corsOptions.ts
@@ -0,0 +1,22 @@
+import { Callback } from "mongoose";
+import allowedOrigins from "./allowedOrigins";
+import { CorsOptions } from "cors";
+
+type OriginFunction = (
+  origin: string | undefined,
+  callback: (err: Error | null, allow?: boolean) => void
+) => void;
+
+const corsOptions: CorsOptions = {
+  origin: ((origin: string, callback: Callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS! "), false);
+    }
+  }) as OriginFunction,
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+export default corsOptions;
--- server/server.ts
+++ server/server.ts
@@ -12,6 +12,7 @@ import mongoose, { Callback } from "mongoose";
 import { logger, logEvents } from "./middleware/logger";
 import errorHandler from "./middleware/errorHandler"; 
 import connectDB from './config/dbConnection';
+import corsOptions from './config/corsOptions';
 
 
 /* CONFIGURATIONS */
@@ -29,7 +30,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 /* FILE STORAGE */
