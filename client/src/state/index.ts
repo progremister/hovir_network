4ad434d0be305819360fcb7eb1e79019aaf5e127
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-07-02 14:14:34 +0200

    Fixed exports

--- client/src/state/index.ts
+++ client/src/state/index.ts
@@ -45,6 +45,6 @@ export const authSlice = createSlice({
   },
 });
 
-export const { setMode, setLogin, setPosts, setPost } = authSlice.actions;
+export const { setMode, setLogin, setLogout, setPosts, setPost } = authSlice.actions;
 
 export default authSlice.reducer;
