ca9aa804572138ec3fce41b93bac094033b8f2c0
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-07-25 21:24:43 +0200

    Created the user widget and added to the home page

--- client/src/scenes/homePage/HomePage.tsx
+++ client/src/scenes/homePage/HomePage.tsx
@@ -1,12 +1,35 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import Navbar from "../../components/Navbar";
+import { useSelector } from "react-redux";
+
+import { IState } from "../../constants";
+import UserWidget from "../widgets/UserWidget";
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width: 850px)");
+  const { _id, picturePath } = useSelector((state: IState) => state.user);
+
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        p="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ client/src/scenes/widgets/UserWidget.tsx
@@ -0,0 +1,144 @@
+import {
+  ManageAccountsOutlined,
+  EditOutlined,
+  LocationOnOutlined,
+  WorkOutlineOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import UserImage from "../../components/UserImage";
+import FlexBetween from "../../components/FlexBetween";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import { IState } from "../../constants";
+
+type UserWidgetProps = { userId: string; picturePath: string };
+const UserWidget = ({ userId, picturePath }: UserWidgetProps) => {
+  const [user, setUser] = useState(null);
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const token = useSelector((state: IState) => state.token);
+
+  const dark = theme.palette.neutral.dark;
+  const medium = theme.palette.neutral.medium;
+  const main = theme.palette.neutral.main;
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3030/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) {
+    return null;
+  }
+
+  const {
+    firstName,
+    lastName,
+    friends,
+    location,
+    occupation,
+    followers,
+    impressions,
+  } = user;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.5rem"
+        onClick={() => navigate(`/profile/${userId}`)}
+      >
+        <FlexBetween gap="1rem">
+          <UserImage image={picturePath} size="35px" />
+          <Box>
+            <Typography
+              variant="h4"
+              color={dark}
+              fontWeight="500"
+              sx={{
+                "&:hover": {
+                  color: theme.palette.primary.light,
+                  cursor: "pointer",
+                },
+              }}
+            >
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}>{followers.length} followers</Typography>
+            <Typography color={medium}>{friends.length} friends</Typography>
+          </Box>
+        </FlexBetween>
+        <ManageAccountsOutlined />
+      </FlexBetween>
+
+      <Divider />
+
+      <Box p="1rem 0">
+        <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+          <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={medium}>{location}</Typography>
+        </Box>
+        <Box display="flex" alignItems="center" gap="1rem">
+          <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={medium}>{occupation}</Typography>
+        </Box>
+      </Box>
+
+      <Divider />
+
+      <Box p="1rem 0">
+        <FlexBetween>
+          <Typography color={medium}>Impressions of your post</Typography>
+          <Typography color={main} fontWeight="500">
+            {impressions}
+          </Typography>
+        </FlexBetween>
+      </Box>
+
+      <Divider />
+
+      <Box p="1rem 0">
+        <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+          Social Profiles
+        </Typography>
+        <FlexBetween gap="1rem" mb="0.5rem">
+          <FlexBetween gap="1rem">
+            <img src="../assets/linkedin.png" alt="linkedin" />
+            <Box>
+              <Typography color={main} fontWeight="500">
+                LinkedIn
+              </Typography>
+              <Typography color={medium}>Network Platform</Typography>
+            </Box>
+          </FlexBetween>
+          <EditOutlined sx={{ color: main }} />
+        </FlexBetween>
+        <FlexBetween gap="1rem">
+          <FlexBetween gap="1rem">
+            <img src="../assets/twitter.png" alt="twitter" />
+            <Box>
+              <Typography color={main} fontWeight="500">
+                Twitter
+              </Typography>
+              <Typography color={medium}>Social Network</Typography>
+            </Box>
+          </FlexBetween>
+          <EditOutlined sx={{ color: main }} />
+        </FlexBetween>
+      </Box>
+    </WidgetWrapper>
+  );
+};
+
+export default UserWidget;
