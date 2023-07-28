264c4584c223fcab0227888f03f735156cf15601
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-07-28 12:18:57 +0200

    Added the callback function to JWT verification

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,38 +1,34 @@
 import { Request, Response, NextFunction } from "express";
-import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import jwt, { JwtPayload, Secret, VerifyErrors } from "jsonwebtoken";
 
-interface IRequest extends Request {
+export interface IRequest extends Request {
   user: string | JwtPayload;
 }
 
-const verifyToken = async (
-  req: IRequest,
-  resp: Response,
-  next: NextFunction
-) => {
-  let token = req.header("Authorization");
-  if (!token) {
-    return resp.status(403).json({ message: "Access denied!" });
+const verifyToken = (req: IRequest, resp: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+
+  if (!authHeader?.toString().startsWith("Bearer ")) {
+    return resp.status(401).json({ message: "Unauthorized!" });
   }
 
-  if (token.startsWith("Bearer ")) {
-    token = token.slice(7, token.length).trimStart();
-  }
+  const token = authHeader.toString().split(" ")[1];
 
   const verified = jwt.verify(
     token,
-    process.env.ACCESS_TOKEN_SECRET as Secret
+    process.env.ACCESS_TOKEN_SECRET as Secret,
+    async (err: VerifyErrors | null, decoded: any ) => {
+      if (err) {
+        return resp.status(403).json({ message: "Forbidden!" });
+      }
+
+      req.user = decoded._id;
+      next();
+    }
   );
-  req.user = verified;
-  next();
 };
 
-//middleware wrapper for verifyJWT
-const verifyJWTMiddleware = (
-  req: Request,
-  resp: Response,
-  next: NextFunction
-) => {
+const verifyJWTMiddleware = (req: Request, resp: Response, next: NextFunction) => {
   verifyToken(req as IRequest, resp, next);
 };
 
