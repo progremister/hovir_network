927c27c14c1c26cc9e95215f073a811e945eb854
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-08-06 01:11:08 +0200

    Developed the profile page

--- client/src/scenes/profilePage/ProfilePage.tsx
+++ client/src/scenes/profilePage/ProfilePage.tsx
@@ -1,9 +1,65 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { Box, useMediaQuery } from "@mui/material";
+import Navbar from "../../components/Navbar";
+import UserWidget from "../widgets/UserWidget";
+import PostsWidget from "../widgets/PostsWidget";
+import ConnectsListWidget from "../widgets/ConnectsListWidget";
+import { IState } from "../../constants";
+
 const ProfilePage = () => {
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state: IState) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:800px)");
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3030/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); //eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) return null;
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        p="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="2rem"
+        justifyContent="center"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={userId} picturePath={user.picturePath} />
+          <Box>
+            {user.connects.length > 0 && (
+              <>
+                <Box m="2rem 0" />
+                <ConnectsListWidget userId={userId} />
+              </>
+            )}
+          </Box>
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <PostsWidget userId={userId} isProfile />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 export default ProfilePage;
