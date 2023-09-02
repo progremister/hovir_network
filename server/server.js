9c815b830127086c07a688aec18b80a78a2c4899
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-09-02 23:17:48 +0200

    Deployment bugfix

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-   origin: "https://hovir.netlify.app/"
+   origin: "https://hovir.vercel.app"
 }));
 
 // app.use(function (req, res, next) {
